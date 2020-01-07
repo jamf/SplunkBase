48f5fddf146c910d345f30529ae06d085c2d698a
kyle.pazandak <kyle.pazandak@jamf.com>
2020-01-07 11:02:01 -0600

    Initial Commit
    Initial Commit

---
+++ bin/JAMF_Pro_addon_for_splunk_rh_jamf.py
@@ -0,0 +1,135 @@
+
+import jamf_pro_addon_for_splunk_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    DataInputModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'interval',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.Pattern(
+            regex=r"""^\-[1-9]\d*$|^\d*$""", 
+        )
+    ), 
+    field.RestField(
+        'index',
+        required=True,
+        encrypted=False,
+        default='default',
+        validator=validator.String(
+            max_len=80, 
+            min_len=1, 
+        )
+    ), 
+    field.RestField(
+        'name_of_the_modular_input',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'jss_url',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'username',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'password',
+        required=True,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'api_call',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'search_name',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'custom_host_name',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'custom_index_name',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=8192, 
+            min_len=0, 
+        )
+    ), 
+
+    field.RestField(
+        'disabled',
+        required=False,
+        validator=None
+    )
+
+]
+model = RestModel(fields, name=None)
+
+
+
+endpoint = DataInputModel(
+    'jamf',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
